df74b5198fbf13e2d42708f53c02a093fbbfe919
sunil <sunil.dn.arya@gmail.com>
2021-09-17 00:10:19 +0530

    header is made responsive

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
-// import FullLogo from '../../assets/blissfairy-full-logo.png';
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+
+//import FullLogo from '../../assets/blissfairy-full-logo.png';
 // import IconLogo from '../../assets/blissfairy-icon-blue.png';
 
-const Navbar = ({setLoggedIn}) => {
-
+const Navigationbar = ({ setLoggedIn }) => {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-white p-6 shadow-md px-36">
-      {/* <div>
-        <img src={FullLogo} alt="" className="w-40 -my-3" />
-      </div> */}
-      <p className="text-md text-bliss-blue font-extrabold">Birthday Project</p>
-      <ul className="flex">
-        <li><Link to="/about" className="text-bliss-blue">Our Mission</Link></li>
-        <li><Button className="rounded-md bg-bliss-blue text-white px-5 ml-12 font-extrabold p-2 text-sm" onClick={()=>{setLoggedIn(true)}}>Provider Login</Button></li>
-      </ul>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Birthday Project</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/about">Our Mission</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">
+              <Button
+                className="rounded-md bg-bliss-blue text-white px-5 ml-12 font-extrabold p-2 text-sm"
+                onClick={() => {
+                  setLoggedIn(true);
+                }}
+              >
+                {" "}
+                Login
+              </Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default Navigationbar;
